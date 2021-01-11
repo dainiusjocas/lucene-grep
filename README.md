f5878c79975d24d8a00cd4bf3e0afb5883d52316
Dainius Jocas <dainius.jocas@vinted.com>
2021-01-11 03:52:41 +0200

    feat: CLI, STDIN, file patterns

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # lucene-grep
-Grep-like utility based on Lucene Monitor
+Grep-like utility based on [Lucene Monitor](https://lucene.apache.org/core/8_3_0/monitor/index.html).
+
+Very limited. Not compatible with `grep`.
+Example in grep:
+```shell
+grep -n -R --include=\*.{edn,clj} "main" ./
+=>
+./deps.edn:22:   :main-opts   ["-m" "cognitect.test-runner"]}
+./deps.edn:24:  {:main-opts  ["-m" "clj-kondo.main --lint src test"]
+./deps.edn:26:   :jvm-opts   ["-Dclojure.main.report=stderr"]}
+./deps.edn:28:  {:main-opts  ["-m clj.native-image core"
+```
+
+Example in lgrep:
+```shell
+clojure -M -m core main "*.{clj,edn}"
+=>
+./src/core.clj:44:(defn -main [& args]
+./deps.edn:22:   :main-opts   ["-m" "cognitect.test-runner"]}
+./deps.edn:24:  {:main-opts  ["-m" "clj-kondo.main --lint src test"]
+./deps.edn:28:  {:main-opts  ["-m clj.native-image core"
+```
+
+Supports input from STDIN:
+```shell
+cat README.md | clojure -M -m core --slop=4 -h "monitor lucene"
+```
+
+```shell
+clojure -M -m core --case-sensitive\?=false --ascii-fold\?=true --stem\?=true --slop=4 --tokenizer=whitespace "lucene" **/*.md
+```
+
+# Supported options
+```shell
+Lucene Monitor based grep-like utility. Usage:
+      --case-sensitive? CASE_SENSITIVE  false  If text should be case sensitive
+      --ascii-fold? ASCII_FOLDED        true   if text should be ascii folded
+      --stem? STEMMED                   true   if text should be stemmed
+      --slop SLOP                       0      How far can be words from each other
+      --stemmer STEMMER                        Which stemmer to use for stemming
+      --tokenizer TOKENIZER                    Tokenizer to use
+  -h, --help
+```
+
+# Supported tokenizers
+TODO
+
+# Supported stemmers
+TODO
--- src/core.clj
+++ src/core.clj
@@ -1,29 +1,93 @@
 (ns core
-  (:require [beagle.phrases :as phrases])
-  (:gen-class))
+  (:require [clojure.java.io :as io]
+            [clojure.tools.cli :as cli]
+            [beagle.phrases :as phrases])
+  (:gen-class)
+  (:import (java.nio.file FileSystems)
+           (java.io File BufferedReader)))
 
-(defn highlighted-text [text]
-  (str \ "[1;31m" text \"[0m"))
+(defn get-files [^String glob]
+  (let [grammar-matcher (.getPathMatcher
+                          (FileSystems/getDefault)
+                          (str "glob:" glob))]
+    (->> "."
+         io/file
+         file-seq
+         (filter #(.isFile %))
+         (filter #(.matches grammar-matcher (.getFileName (.toPath %))))
+         (mapv #(.getPath ^File %)))))
 
-(defn match [query-string file-path]
-  (let [dictionary [{:text            query-string
-                     :id              "1"
-                     :case-sensitive? false
-                     :ascii-fold?     true
-                     :stem?           true
-                     :stemmer         (keyword "english")}]]
-    (with-open [rdr (clojure.java.io/reader file-path)]
-      (doseq [[line-str line-number] (map (fn [line-str line-number] [line-str line-number])
-                                          (line-seq rdr) (range))]
-        (let [highlighter-fn (phrases/highlighter dictionary)]
-          (when-let [[first-highlight & _] (seq (highlighter-fn line-str))]
-            (println
-              (format "%s  %s%s%s"
-                      line-number
-                      (subs line-str 0 (:begin-offset first-highlight))
-                      (highlighted-text (:text first-highlight))
-                      (subs line-str (:end-offset first-highlight))))))))))
+(defn red-text [text]
+  (str \ "[1;31m" text \ "[0m"))
+
+(defn purple-text [text]
+  (str \ "[0;35m" text \ "[0m"))
+
+(defn green-text [text]
+  (str \ "[0;32m" text \ "[0m"))
+
+(defn match-lines [highlighter-fn file-path lines]
+  (doseq [[line-str line-number] (map (fn [line-str line-number] [line-str line-number])
+                                      lines (range))]
+    (when-let [[first-highlight & _] (seq (highlighter-fn line-str))]
+      (println
+        (format "%s:%s:%s%s%s"
+                (purple-text (or file-path "*STDIN*"))
+                (green-text (inc line-number))
+                (subs line-str 0 (:begin-offset first-highlight))
+                (red-text (:text first-highlight))
+                (subs line-str (:end-offset first-highlight)))))))
+
+(defn match [query-string files-pattern options]
+  (let [dictionary [(merge {:text            query-string
+                            :case-sensitive? false
+                            :ascii-fold?     true
+                            :stem?           true
+                            :slop            0
+                            :tokenizer       :standard
+                            :stemmer         :english}
+                           options)]
+        highlighter-fn (phrases/highlighter dictionary)]
+    (if files-pattern
+      (doseq [path (get-files files-pattern)]
+        (with-open [rdr (io/reader path)]
+          (match-lines highlighter-fn path (line-seq rdr))))
+      (when (.ready *in*)
+        (match-lines highlighter-fn nil (line-seq (BufferedReader. *in*)))))))
+
+(def cli-options
+  [[nil "--case-sensitive? CASE_SENSITIVE" "If text should be case sensitive"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default false]
+   [nil "--ascii-fold? ASCII_FOLDED" "if text should be ascii folded"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   [nil "--stem? STEMMED" "if text should be stemmed"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   [nil "--slop SLOP" "How far can be words from each other"
+    :parse-fn #(Integer/parseInt %)
+    :default 0]
+   [nil "--stemmer STEMMER" "Which stemmer to use for stemming"
+    :parse-fn #(keyword %)]
+   [nil "--tokenizer TOKENIZER" "Tokenizer to use"
+    :parse-fn #(keyword %)]
+   ["-h" "--help"]])
+
+(comment
+  (cli/parse-opts ["--slop=2" "--tokenizer=standard" "--stem?=false" "--stemmer=english" "--case-sensitive?=true"] cli-options)
+  (cli/parse-opts ["test"] cli-options))
 
 (defn -main [& args]
-  (match (nth args 0) (nth args 1))
+  (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
+    (when (seq errors)
+      (println "Errors:" errors)
+      (System/exit 1))
+    (when (:help options)
+      (println "Lucene Monitor based grep-like utility. Usage:")
+      (println summary)
+      (System/exit 1))
+    (if (= 2 (count arguments))
+      (match (nth arguments 0) (nth arguments 1) options)
+      (match (nth arguments 0) nil options)))
   (System/exit 0))
