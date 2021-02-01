dcc380741eb3bf9ce590a4da6f1de5a9bd978f9f
Dainius Jocas <dainius.jocas@pm.me>
2021-02-01 22:20:40 +0200

    Flag not to split text file into lines (#35)
    * feat: flag not to split the text file into lines
    * mention a flag to the changelog

--- src/lmgrep/cli.clj
+++ src/lmgrep/cli.clj
@@ -62,6 +62,8 @@
    [nil "--excludes EXCLUDES" "A GLOB that filters out files that were matched with a GLOB"]
    [nil "--skip-binary-files" "If a file that is detected to be binary should be skipped. Available for Linux and MacOS only."
     :default false]
+   [nil "--[no-]split" "If a file (or STDIN) should be split by newline."
+    :default true]
    ;[nil "--slop SLOP" "How far can be words from each other"
    ; :parse-fn #(Integer/parseInt %)
    ; :default 0]
