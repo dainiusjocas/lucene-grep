ffb13ee3af3a4e207dbfaf889dab9444f05cda5c
Dainius Jocas <dainius.jocas@vinted.com>
2021-02-07 23:06:09 +0200

    chore: bumped Lucene 8.8.0; clojure tools logging 1.1.0

--- test/lmgrep/grep_test.clj
+++ test/lmgrep/grep_test.clj
@@ -1,12 +1,12 @@
 (ns lmgrep.grep-test
   (:require [clojure.test :refer [deftest is testing]]
             [lmgrep.grep :as grep]
-            [beagle.phrases :as phrases]))
+            [lmgrep.lucene :as lucene]))
 
 (deftest highlighting-test
   (testing "coloring the output"
     (let [query-string "text"
-          highlighter-fn (phrases/highlighter [{:text query-string}])
+          highlighter-fn (lucene/highlighter [{:text query-string}])
           text "prefix text suffix"]
       (is (= (str "prefix " \ "[1;31mtext" \ "[0m suffix")
              (grep/highlight-line text (highlighter-fn text) {}))))))
