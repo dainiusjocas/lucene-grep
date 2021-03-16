9b177808e626a8c5d8a8ba07c4d57a1383eab037
Dainius Jocas <dainius.jocas@vinted.com>
2021-03-16 18:39:53 +0200

    text: write tests with stdin and stdout overriden

--- test/lmgrep/grep_test.clj
+++ test/lmgrep/grep_test.clj
@@ -1,5 +1,7 @@
 (ns lmgrep.grep-test
   (:require [clojure.test :refer [deftest is testing]]
+            [clojure.string :as str]
+            [lmgrep.formatter :as formatter]
             [lmgrep.grep :as grep]
             [lmgrep.lucene :as lucene]))
 
@@ -9,4 +11,33 @@
           highlighter-fn (lucene/highlighter [{:text query-string}])
           text "prefix text suffix"]
       (is (= (str "prefix " \ "[1;31mtext" \ "[0m suffix")
-             (grep/highlight-line text (highlighter-fn text) {}))))))
+             (formatter/highlight-line text (highlighter-fn text) {}))))))
+
+(deftest grepping-file
+  (let [file "test/resources/test.txt"
+        query "fox"
+        options {:split true :pre-tags ">" :post-tags "<" :template "{{highlighted-line}}"}]
+    (is (= "The quick brown >fox< jumps over the lazy dog"
+           (str/trim
+             (with-out-str
+               (grep/grep [query] file nil options)))))))
+
+(deftest grepping-stdin
+  (let [text-from-stdin "The quick brown fox jumps over the lazy dog"
+        query "fox"
+        options {:split true :pre-tags ">" :post-tags "<" :template "{{highlighted-line}}"}]
+    (is (= "The quick brown >fox< jumps over the lazy dog"
+           (with-in-str text-from-stdin
+                        (str/trim
+                          (with-out-str
+                            (grep/grep [query] nil nil options))))))))
+
+(deftest grepping-multiple-queries
+  (let [text-from-stdin "The quick brown fox jumps over the lazy dog"
+        queries ["fox" "dog"]
+        options {:split true :pre-tags ">" :post-tags "<" :template "{{highlighted-line}}"}]
+    (is (= "The quick brown >fox< jumps over the lazy >dog<"
+           (with-in-str text-from-stdin
+                        (str/trim
+                          (with-out-str
+                            (grep/grep queries nil nil options))))))))
