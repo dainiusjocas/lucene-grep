7c5d79cfbdf01f5b2041217d39374dc28d8c959c
Dainius Jocas <dainius.jocas@gmail.com>
2021-04-06 20:43:07 +0300

    refactor: put into a class

--- examples/nodejs-percolator/percolator.js
+++ examples/nodejs-percolator/percolator.js
@@ -1,23 +1,43 @@
 const {spawn} = require('child_process');
 
-const grep = spawn('lmgrep', ['test'] );
+class Percolator {
 
-grep.stdout.on('data', (data) => {
-    console.log(`>>>>: ${data}`);
-});
+    constructor(query) {
+        this.a = null;
+        this.grep = spawn('lmgrep', [query] );
 
-grep.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
+        this.grep.stdout.on('data', (data) => {
+            console.log(`>>>><<<: ${data}`);
+            this.a = data;
+        });
 
-grep.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+        this.grep.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
 
-grep.stdin.setEncoding('utf-8');
+        this.grep.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
 
-grep.stdin.write("foo test bar\n");
+        this.grep.stdin.setEncoding('utf-8');
+    }
 
-grep.stdin.end();
+    percolate(text) {
+        this.grep.stdin.write(text + "\n");
+        return this.a;
+    }
+
+    close() {
+        this.grep.stdin.end();
+    }
+}
+
+const percolator = new Percolator('test');
+
+resp = percolator.percolate("foo test bar")
+
+console.log(">>>>" + resp);
+
+percolator.close();
 
 console.log("Done");
