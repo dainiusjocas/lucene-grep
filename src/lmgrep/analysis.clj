3370a642b3ae41eee583ee6e308309cda6b71631
Dainius Jocas <dainius.jocas@gmail.com>
2021-11-07 23:08:10 +0200

    feat: take into account the config-dir

--- src/lmgrep/analysis.clj
+++ src/lmgrep/analysis.clj
@@ -12,10 +12,22 @@
                            :config-dir (get options :config-dir)))))
           {} (get conf :analyzers)))
 
+(defn config-file-path
+  "Checks and returns a file if the there is such a file provided as config-path.
+  If there is no such file then checks if config-dir is provided is the is
+  such a directory. If there is then tries to find a config-file there."
+  [file-path options]
+  (let [config-file (io/file file-path)]
+    (if (and (.exists config-file) (.isFile config-file))
+      config-file
+      (let [config-dir (get options :config-dir)]
+        (when (and config-dir) (.isDirectory (io/file config-dir))
+          (io/file config-dir file-path))))))
+
 (defn read-analysis-conf-from-file
   "Given a file returns a hashmap {analyzer_name custom_analyzer}"
   [^String file-path options]
-  (let [^File input-file (io/file file-path)]
+  (let [^File input-file (config-file-path file-path options)]
     (if (.isFile input-file)
       (let [conf (with-open [is (io/input-stream input-file)]
                    (json/read-value is json/keyword-keys-object-mapper))]
--- test/lmgrep/analysis_file_test.clj
+++ test/lmgrep/analysis_file_test.clj
@@ -11,3 +11,13 @@
     (let [[analyzer-name analyzer] (first analyzers)]
       (is (= "some-custom-analyzer" analyzer-name))
       (is (instance? Analyzer analyzer)))))
+
+(deftest read-analyzers-with-config-dir
+  (let [opts {:config-dir "test/resources/"}
+        file "analyzers.json"
+        analyzers (a/read-analysis-conf-from-file file opts)]
+    (is (map? analyzers))
+    (is (= 1 (count analyzers)))
+    (let [[analyzer-name analyzer] (first analyzers)]
+      (is (= "some-custom-analyzer" analyzer-name))
+      (is (instance? Analyzer analyzer)))))
