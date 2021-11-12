80e43670ea1422882c5bc5971b120d1f1db34909
Dainius Jocas <dainius.jocas@gmail.com>
2021-11-12 23:16:52 +0200

    Tests should check for files in test dir (#130)
    * fix: tests should check for files in test dir

--- test/lmgrep/fs_test.clj
+++ test/lmgrep/fs_test.clj
@@ -7,18 +7,18 @@
     (is (= ["deps.edn"] (into [] (fs/filter-files files))))))
 
 (deftest excludes-files
-  (is (> (count (fs/get-files "docs/**.*" {}))
-         (count (fs/get-files "docs/**.*" {:excludes "**.png"})))))
+  (is (> (count (fs/get-files "test/**.*" {}))
+         (count (fs/get-files "test/**.*" {:excludes "**.xml"})))))
 
 (deftest skipping-binary-files
-  (is (> (count (fs/get-files "docs/**.*" {}))
-         (count (fs/get-files "docs/**.*" {:skip-binary-files true})))))
+  (is (> (count (fs/get-files "test/**.*" {}))
+         (count (fs/get-files "test/**.*" {:skip-binary-files true})))))
 
 (deftest binary-file-shipping
   (when (contains? #{"Linux" "Mac OS X"} (System/getProperty "os.name"))
     ; NOTE: Windows doesn't support this feature
-    ; All .png files in docs should be binary
-    (is (= [] (fs/get-files "docs/**.png" {:skip-binary-files true})))))
+    ; All .png files in test dir should be binary
+    (is (= [] (fs/get-files "test/**.png" {:skip-binary-files true})))))
 
 (deftest infering-root-dir
   (is (= "." (fs/infer-root-folder ".env")))
