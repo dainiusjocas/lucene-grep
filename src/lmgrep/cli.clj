444cef0eaba97e2a55e9609f4b2389edd95821d1
Dainius Jocas <dainius.jocas@gmail.com>
2022-01-14 18:40:31 +0200

    refactor: remove text analysis flags from parsed CLI options

--- src/lmgrep/cli.clj
+++ src/lmgrep/cli.clj
@@ -3,11 +3,19 @@
             [lmgrep.cli.parser :as parser]
             [lmgrep.cli.analysis-conf :as ac]))
 
+(defn prepare-analysis [options]
+  (if (empty? (get-in options [:options :analysis]))
+    (assoc-in options [:options :analysis] (ac/prepare-analysis-configuration ac/default-text-analysis options))
+    options))
+
+(defn remove-text-analysis-flags [options]
+  (apply dissoc options ac/analysis-keys))
+
 (defn handle-args [args]
-  (let [opts (cli/parse-opts args parser/cli-options)]
-    (if (empty? (get-in opts [:options :analysis]))
-      (assoc-in opts [:options :analysis] (ac/prepare-analysis-configuration ac/default-text-analysis opts))
-      opts)))
+  (-> args
+      (cli/parse-opts parser/cli-options)
+      prepare-analysis
+      (update-in [:options] remove-text-analysis-flags)))
 
 (comment
   (lmgrep.cli/handle-args ["--tokenizer=standard" "--stem?=false" "--stemmer=english" "--case-sensitive?=true"]))
