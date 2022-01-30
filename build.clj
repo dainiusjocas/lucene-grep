8d25b83c01e1327537c9a25c11e9525bfb2bc00f
Dainius Jocas <dainius.jocas@gmail.com>
2022-01-30 13:14:03 +0200

    YES: found a way to package local deps so that the resources are properly merged

--- build.clj
+++ build.clj
@@ -1,7 +1,31 @@
 (ns build
-  (:require [clojure.tools.cli.api :as c]))
+  (:require [clojure.tools.cli.api :as c]
+            [clojure.tools.build.api :as b]
+            [script.env-profiles :as p]))
 
 (defn prep-deps [& _]
   (println "Preparing transitive dependencies")
+  ; TODO: Can it be made to work with multiple aliases in one pass
   (c/prep (assoc (c/basis {:aliases [:raudikko]}) :force true))
   (c/prep (assoc (c/basis {:aliases [:snowball-token-filters]}) :force true)))
+
+(def class-dir "target/classes")
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uberjar [args]
+  (println "building uberjar with" args)
+  (p/profiles)
+  (let [basis (b/create-basis {:project "deps.edn"
+                               :aliases [:raudikko :snowball-token-filters :stempel]})]
+   (b/copy-dir {:src-dirs   ["src" "resources"]
+                :target-dir class-dir})
+   (b/compile-clj {:basis     basis
+                   :src-dirs  ["src"]
+                   :class-dir class-dir})
+   (b/uber {:class-dir         class-dir
+            :uber-file         "target/lmgrep.jar"
+            :basis             basis
+            :main              'lmgrep.core
+            :conflict-handlers {}})))
---
+++ script/env_profiles.clj
@@ -0,0 +1,6 @@
+(ns script.env-profiles)
+
+(defn profiles
+  "Checks the relevant environment variables and returns a list of alias keywords."
+  []
+  (println "FOOOOOOOOOOOOOOOOO"))
