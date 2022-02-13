10770829838d13ee4ba4a8096f4c2ec8e678d5ba
Dainius Jocas <dainius.jocas@vinted.com>
2022-02-13 20:31:37 +0200

    refactor: do not recreate thread pools for every file

--- src/lmgrep/unordered.clj
+++ src/lmgrep/unordered.clj
@@ -5,35 +5,40 @@
            (java.util.concurrent ThreadPoolExecutor TimeUnit Executors LinkedBlockingQueue ThreadPoolExecutor$CallerRunsPolicy ExecutorService)
            (lmgrep.matching LineNrStr)))
 
+(defn shutdown-thread-pool-executors [& executors]
+  (doseq [^ExecutorService executor executors]
+    (.shutdown executor)
+    (.awaitTermination executor 60 TimeUnit/SECONDS)))
+
+(defn thread-pool-executor [^Integer concurrency ^Integer queue-size]
+  (ThreadPoolExecutor.
+    concurrency concurrency
+    0 TimeUnit/MILLISECONDS
+    (LinkedBlockingQueue. queue-size)
+    (Executors/defaultThreadFactory)
+    (ThreadPoolExecutor$CallerRunsPolicy.)))
+
 (defn consume-reader
-  "TODO:"
-  [reader ^PrintWriter writer matcher-fn concurrency queue-size with-empty-lines]
-  (let [^ExecutorService analyzer-pool (ThreadPoolExecutor.
-                                         concurrency concurrency
-                                         0 TimeUnit/MILLISECONDS
-                                         (LinkedBlockingQueue. ^Integer queue-size)
-                                         (Executors/defaultThreadFactory)
-                                         (ThreadPoolExecutor$CallerRunsPolicy.))
-        ^ExecutorService writer-pool (Executors/newSingleThreadExecutor)]
-    (with-open [^BufferedReader rdr reader]
-      (loop [^String line (.readLine rdr)
-             line-nr 0]
-        (when-not (nil? line)
-          (.execute analyzer-pool
-                    ^Runnable (fn []
-                                (let [out-str (matcher-fn (LineNrStr. line-nr line))]
-                                  (if (.equals "" out-str)
-                                    (when with-empty-lines
-                                      (.execute writer-pool
-                                                ^Runnable (fn [] (.println writer out-str))))
-                                    (.execute writer-pool
-                                              ^Runnable (fn [] (.println writer out-str)))))))
-          (recur (.readLine rdr) (inc line-nr))))
-      (.shutdown analyzer-pool)
-      (.awaitTermination analyzer-pool 60 TimeUnit/SECONDS)
-      (.shutdown writer-pool)
-      (.awaitTermination writer-pool 60 TimeUnit/SECONDS)
-      (.flush writer))))
+  "Given a Reader iterates over lines and sends them to the
+  matcher-thread-pool-executor for further handling."
+  [reader ^PrintWriter writer
+   matcher-fn
+   matcher-thread-pool-executor writer-thread-pool-executor
+   with-empty-lines]
+  (with-open [^BufferedReader rdr reader]
+    (loop [^String line (.readLine rdr)
+           line-nr 0]
+      (when-not (nil? line)
+        (.execute matcher-thread-pool-executor
+                  ^Runnable (fn []
+                              (let [^String out-str (matcher-fn (LineNrStr. line-nr line))]
+                                (if (.equals "" out-str)
+                                  (when with-empty-lines
+                                    (.execute writer-thread-pool-executor
+                                              ^Runnable (fn [] (.println writer out-str))))
+                                  (.execute writer-thread-pool-executor
+                                            ^Runnable (fn [] (.println writer out-str)))))))
+        (recur (.readLine rdr) (inc line-nr))))))
 
 (defn grep [file-paths-to-analyze highlighter-fn options]
   (let [reader-buffer-size (get options :reader-buffer-size 8192)
@@ -41,12 +46,22 @@
         concurrency (get options :concurrency (.availableProcessors (Runtime/getRuntime)))
         queue-size (get options :queue-size 1024)
         with-empty-lines (get options :with-empty-lines)
-        ^PrintWriter writer (PrintWriter. (BufferedWriter. *out* print-writer-buffer-size))]
+        ^PrintWriter writer (PrintWriter. (BufferedWriter. *out* print-writer-buffer-size))
+        ^ExecutorService matcher-thread-pool-executor (thread-pool-executor concurrency queue-size)
+        ^ExecutorService writer-thread-pool-executor (Executors/newSingleThreadExecutor)]
     (if (empty? file-paths-to-analyze)
       (let [reader (BufferedReader. *in* reader-buffer-size)
             matcher-fn (matching/matcher-fn highlighter-fn nil options)]
-        (consume-reader reader writer matcher-fn concurrency queue-size with-empty-lines))
+        (consume-reader reader writer
+                        matcher-fn
+                        matcher-thread-pool-executor writer-thread-pool-executor
+                        with-empty-lines))
       (doseq [^String path file-paths-to-analyze]
         (let [reader (io/reader path)
               matcher-fn (matching/matcher-fn highlighter-fn path options)]
-          (consume-reader reader writer matcher-fn concurrency queue-size with-empty-lines))))))
+          (consume-reader reader writer
+                          matcher-fn
+                          matcher-thread-pool-executor writer-thread-pool-executor
+                          with-empty-lines))))
+    (shutdown-thread-pool-executors matcher-thread-pool-executor writer-thread-pool-executor)
+    (.flush writer)))
