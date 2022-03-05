4f592490fc61acd06ff45208770b457d4bf336fe
Dainius Jocas <dainius.jocas@gmail.com>
2022-03-05 21:15:00 +0200

    chore: docs

--- src/lmgrep/lucene/custom_analyzer.clj
+++ src/lmgrep/lucene/custom_analyzer.clj
@@ -3,25 +3,29 @@
            (java.io File)
            (java.nio.file Path)
            (org.apache.lucene.analysis.custom CustomAnalyzer CustomAnalyzer$Builder)
-           (org.apache.lucene.analysis Analyzer TokenizerFactory CharFilterFactory TokenFilterFactory)))
+           (org.apache.lucene.analysis Analyzer CharFilterFactory TokenFilterFactory TokenizerFactory)))
 
-(defn stringify [m]
+(defn- stringify [m]
   (reduce-kv (fn [acc k v] (assoc acc (name k) (str v))) {} m))
 
-(def tokenizer-name->class
+(defn load-tokenizer-factories []
   (reduce (fn [acc ^String tokenizer-name]
             (assoc acc tokenizer-name (TokenizerFactory/lookupClass tokenizer-name)))
           {} (TokenizerFactory/availableTokenizers)))
 
-(def char-filter-name->class
+(defn load-char-filter-factories []
   (reduce (fn [acc ^String char-filter-name]
             (assoc acc char-filter-name (CharFilterFactory/lookupClass char-filter-name)))
           {} (CharFilterFactory/availableCharFilters)))
 
-(def token-filter-name->class
+(defn load-token-filter-factories []
   (reduce (fn [acc ^String token-filter-name]
             (assoc acc token-filter-name (TokenFilterFactory/lookupClass token-filter-name)))
           {} (TokenFilterFactory/availableTokenFilters)))
+
+(def tokenizer-name->class (load-tokenizer-factories))
+(def char-filter-name->class (load-char-filter-factories))
+(def token-filter-name->class (load-token-filter-factories))
 
 (def DEFAULT_TOKENIZER_NAME "standard")
 
@@ -39,6 +43,35 @@
                   (sort (keys factories)))))))
 
 (defn create
+  "Constructs a Lucene Analyzer using the CustomAnalyzer builder.
+   Under the hood it uses the factory classes TokenizerFactory, TokenFilterFactory, and CharFilterFactory.
+   The factories are loaded with java.util.ServiceLoader.
+
+   Factory description is of shape:
+   `
+   {:name String
+    :args Map}
+   `
+
+   If needed factories can be passed as arguments in shape:
+   `
+   {STRING CLASS}
+   `
+
+   Example:
+   `
+   {:tokenizer {:name \"standard\", :args {:maxTokenLength 4}}
+    :char-filters [{:name \"patternReplace\" :args {:pattern \"foo\", :replacement \"foo\"}}]
+    :token-filters [{:name \"uppercase\"} {:name \"reverseString\"}]
+    :config-dir \".\"}
+   `
+
+   `opts` can have specified:
+     - config-dir: path to directory from which resources will be loaded, default '.'
+     - char-filters: list of char filter descriptions
+     - tokenizer: tokenizer description, default 'standard' tokenizer
+     - token-filters: list of token filter descriptions
+     - namify-fn: function that changes the string identifier of the service name, e.g. str/lowercase, default: identity"
   (^Analyzer [opts]
    (create opts char-filter-name->class tokenizer-name->class token-filter-name->class))
   (^Analyzer [{:keys [config-dir char-filters tokenizer token-filters namify-fn]}
@@ -90,10 +123,4 @@
                      :args {:pattern "foo"
                             :replacement "foo"}}]
      :token-filters [{:name "uppercase"}
-                     {:name "reverseString"}]})
-
-  (lmgrep.lucene.custom-analyzer/create
-    {:tokenizer {:name "standard"}
-     :char-filters [{:name "patternReplace"
-                     :args {:pattern "foo"
-                            :replacement "bar"}}]}))
+                     {:name "reverseString"}]}))
