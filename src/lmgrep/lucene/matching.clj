217567d022b1ac65a876f7dda03aa8157943c377
Dainius Jocas <dainius.jocas@gmail.com>
2022-05-01 18:45:19 +0300

    refactor: eager matches processing

--- src/lmgrep/lucene/matching.clj
+++ src/lmgrep/lucene/matching.clj
@@ -5,7 +5,8 @@
                                       ScoringMatch
                                       ScoringHighlightsMatch ScoringHighlightsMatch$Hit)
            (org.apache.lucene.document Document Field FieldType)
-           (org.apache.lucene.index IndexOptions)))
+           (org.apache.lucene.index IndexOptions)
+           (java.util Map$Entry Set Iterator)))
 
 (def ^FieldType field-type
   (doto (FieldType.)
@@ -14,42 +15,66 @@
     (.setStoreTermVectors true)
     (.setStoreTermVectorOffsets true)))
 
+(defn ^:private highlights-match->highlights
+  [^HighlightsMatch query-match ^Monitor monitor]
+  (let [^MonitorQuery query (.getQuery monitor (.getQueryId query-match))
+        meta (.getMetadata query)
+        base {:query         (.getQueryString query)
+              :type          (get meta "_type")
+              :dict-entry-id (.getQueryId query-match)
+              :meta          (dissoc (into {} meta) "_type")}
+        hits-iterator (.iterator (.entrySet (.getHits query-match)))
+        highlights (transient [])]
+    (while (.hasNext hits-iterator)
+      (let [^Map$Entry map-entry (.next hits-iterator)      ; key of the map entry is a field name
+            ^Set highlights-match-hits (.getValue map-entry)
+            iterator (.iterator highlights-match-hits)]
+        (while (.hasNext iterator)
+          (conj! highlights (let [^HighlightsMatch$Hit hit (.next iterator)]
+                              (assoc (assoc base :begin-offset (.-startOffset hit))
+                                :end-offset (.-endOffset hit)))))))
+    (persistent! highlights)))
+
+(defn ^:private scoring-highlights-match->highlights
+  [^ScoringHighlightsMatch query-match ^Monitor monitor]
+  (let [^MonitorQuery query (.getQuery monitor (.getQueryId query-match))
+        meta (.getMetadata query)
+        base {:query         (.getQueryString query)
+              :type          (get meta "_type")
+              :dict-entry-id (.getQueryId query-match)
+              :meta          (dissoc (into {} meta) "_type")
+              :score         (.getScore query-match)}
+        hits-iterator (.iterator (.entrySet (.getHits query-match)))
+        highlights (transient [])]
+    (while (.hasNext hits-iterator)
+      (let [^Map$Entry map-entry (.next hits-iterator)      ; key of the map entry is a field name
+            ^Set highlights-match-hits (.getValue map-entry)
+            iterator (.iterator highlights-match-hits)]
+        (while (.hasNext iterator)
+          (conj! highlights (let [^ScoringHighlightsMatch$Hit hit (.next iterator)]
+                              (assoc (assoc base :begin-offset (.-startOffset hit))
+                                :end-offset (.-endOffset hit)))))))
+    (persistent! highlights)))
+
 (defn match-text [^String text ^Monitor monitor field-names]
-  (let [doc (Document.)]
+  (let [combined-highlights (transient [])
+        doc (Document.)]
     (doseq [field-name field-names]
       (.add doc (Field. ^String field-name text field-type)))
-    (mapcat (fn [^HighlightsMatch query-match]
-              (let [^MonitorQuery query (.getQuery monitor (.getQueryId query-match))
-                    meta (.getMetadata query)
-                    base {:query         (.getQueryString query)
-                          :type          (get meta "_type")
-                          :dict-entry-id (.getQueryId query-match)
-                          :meta          (dissoc (into {} meta) "_type")}]
-                (mapcat (fn [[_ ^HighlightsMatch$Hit hit]]
-                          (mapv (fn [^HighlightsMatch$Hit h]
-                                  (assoc base :begin-offset (.-startOffset h)
-                                              :end-offset (.-endOffset h))) hit))
-                        (.getHits query-match))))
-            (.getMatches (.match monitor doc (HighlightsMatch/MATCHER))))))
+    (let [^Iterator miter (.iterator (.getMatches (.match monitor doc (HighlightsMatch/MATCHER))))]
+      (while (.hasNext miter)
+        (reduce conj! combined-highlights (highlights-match->highlights (.next miter) monitor)))
+      (persistent! combined-highlights))))
 
 (defn match-with-scoring-highlights [^String text ^Monitor monitor field-names]
-  (let [doc (Document.)]
+  (let [combined-highlights (transient [])
+        doc (Document.)]
     (doseq [field-name field-names]
       (.add doc (Field. ^String field-name text field-type)))
-    (mapcat (fn [^ScoringHighlightsMatch query-match]
-              (let [^MonitorQuery query (.getQuery monitor (.getQueryId query-match))
-                    meta (.getMetadata query)
-                    base {:query         (.getQueryString query)
-                          :type          (get meta "_type")
-                          :dict-entry-id (.getQueryId query-match)
-                          :meta          (dissoc (into {} meta) "_type")
-                          :score         (.getScore query-match)}]
-                (mapcat (fn [[_ ^ScoringHighlightsMatch$Hit hit]]
-                          (mapv (fn [^ScoringHighlightsMatch$Hit h]
-                                  (assoc base :begin-offset (.-startOffset h)
-                                              :end-offset (.-endOffset h))) hit))
-                        (.getHits query-match))))
-            (.getMatches (.match monitor doc (ScoringHighlightsMatch/MATCHER))))))
+    (let [^Iterator miter (.iterator (.getMatches (.match monitor doc (ScoringHighlightsMatch/MATCHER))))]
+      (while (.hasNext miter)
+        (reduce conj! combined-highlights (scoring-highlights-match->highlights (.next miter) monitor)))
+      (persistent! combined-highlights))))
 
 (defn match-with-score [^String text ^Monitor monitor field-names]
   (let [doc (Document.)]
