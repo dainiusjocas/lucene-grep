ee0ede84584dcaa14b6814d5a2080569038963aa
Dainius Jocas <dainius.jocas@gmail.com>
2023-06-18 12:45:42 +0300

    fix: use system properties and not env vars for compile time flags

--- src/lmgrep/features.clj
+++ src/lmgrep/features.clj
@@ -1,7 +1,11 @@
 (ns lmgrep.features)
 
 ;; excluded by default
-(def raudikko? (= "true" (System/getenv "LMGREP_FEATURE_RAUDIKKO")))
-(def snowball? (= "true" (System/getenv "LMGREP_FEATURE_SNOWBALL")))
-(def stempel? (= "true" (System/getenv "LMGREP_FEATURE_STEMPEL")))
-(def bundled? (= "true" (System/getenv "LMGREP_FEATURE_BUNDLED_ANALYZERS")))
+(def raudikko? (or (= "true" (System/getenv "LMGREP_FEATURE_RAUDIKKO"))
+                   (= "true" (System/getProperty "LMGREP_FEATURE_RAUDIKKO"))))
+(def snowball? (or (= "true" (System/getenv "LMGREP_FEATURE_SNOWBALL"))
+                   (= "true" (System/getProperty "LMGREP_FEATURE_SNOWBALL"))))
+(def stempel? (or (= "true" (System/getenv "LMGREP_FEATURE_STEMPEL"))
+                  (= "true" (System/getProperty "LMGREP_FEATURE_STEMPEL"))))
+(def bundled? (or (= "true" (System/getenv "LMGREP_FEATURE_BUNDLED_ANALYZERS"))
+                  (= "true" (System/getProperty "LMGREP_FEATURE_BUNDLED_ANALYZERS"))))
